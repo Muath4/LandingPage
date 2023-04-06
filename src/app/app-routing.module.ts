b6b943145f2adeef22b832d7b3cfd1049c25f0a8
Muath Alshathri <abogbl4@gmail.com>
2023-04-06 13:39:07 +0300

    update gt pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,9 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
-  { path: '**', redirectTo: '' },
+  { path: 'statistics', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
+  { path: 'main', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
+  { path: '**', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
 
 ];
 
