163a38cf34d7c838c78ce3517b825db680054a49
Muath Alshathri <abogbl4@gmail.com>
2023-04-06 13:51:26 +0300

    update page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
-  { path: 'statistics', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
-  { path: 'main', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
-  { path: '**', loadComponent: () => import('./chart/chart.component').then(mod => mod.ChartComponent) },
+  { path: '**', redirectTo: '' },
 
 ];
 
